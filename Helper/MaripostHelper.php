84c388b620cbfd56b3cb0c197ecb8c8617b93c86
Nick T-bone Kiermaier <nickkiermaier@gmail.com>
2018-07-30 17:42:47 -0500

    add MaripostPHPHelper

---
+++ Helper/MaripostHelper.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 7/30/18
+ * Time: 2:43 PM
+ */
+
+require_once '../vendor/autoload.php';
+
+/**
+ * Generalized function for connecting to the Maripost ESP
+ * EspDocs:
+ * http://api.maropost.com/api
+ * URL: https://app.maropost.com/users/sign_in
+ * Username: Andrew@rxmg.com
+ * Temporary Password: And1234$
+ *
+ * Class MaripostHelper
+ */
+class MaripostHelper{
+
+
+}
--- Helper/MaripostPhpHelper.php
+++ Helper/MaripostPhpHelper.php
@@ -1,24 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 7/30/18
- * Time: 2:43 PM
- */
 
-require_once '../vendor/autoload.php';
 
 /**
- * Generalized function for connecting to the Maripost ESP
- * EspDocs:
- * http://api.maropost.com/api
- * URL: https://app.maropost.com/users/sign_in
- * Username: Andrew@rxmg.com
- * Temporary Password: And1234$
- *
- * Class MaripostHelper
+ * Class MaripostPhpHelper
  */
-class MaripostHelper{
-
+class MaripostPhpHelper{
 
 }
